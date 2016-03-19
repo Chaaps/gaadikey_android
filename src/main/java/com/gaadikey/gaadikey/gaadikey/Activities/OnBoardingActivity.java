f5e3f31e7f4f1d448f2196598d450da0d8921a97
Sreenivasen Ramasubramanian <SREENIVASEN@Sreenivasens-MacBook-Pro.local>
2016-03-19 12:56:18 -0700

    Step 1
    Added the on boarding screen and cleaned up manifest file

---
+++ src/main/java/com/gaadikey/gaadikey/gaadikey/Activities/OnBoardingActivity.java
@@ -0,0 +1,14 @@
+package com.gaadikey.gaadikey.gaadikey.activities;
+
+import android.os.Bundle;
+
+import com.gaadikey.gaadikey.gaadikey.R;
+
+public class OnBoardingActivity extends GaadiKeyActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_on_boarding);
+    }
+}
