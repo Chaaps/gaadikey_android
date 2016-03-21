ef9e50343efe6ba83d2f489d65e249ba06cbdf39
Sreenivasen Ramasubramanian <SREENIVASEN@Sreenivasens-MacBook-Pro.local>
2016-03-21 01:11:43 -0700

    I need to update gitgnore file. But, for now commiting it anyway.

--- src/main/java/com/gaadikey/gaadikey/gaadikey/Activities/OnBoardingActivity.java
+++ src/main/java/com/gaadikey/gaadikey/gaadikey/Activities/OnBoardingActivity.java
@@ -13,6 +13,9 @@ public class OnBoardingActivity extends FragmentActivity {
 
     ViewPager gaadikey_on_boarding_viewpager;
     PagerAdapter gaadikey_pager_adapter;
+    ImageView on_boarding_indicator_image1;
+    ImageView on_boarding_indicator_image2;
+    ImageView on_boarding_indicator_image3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +23,48 @@ public class OnBoardingActivity extends FragmentActivity {
         setContentView(R.layout.activity_on_boarding);
 
         gaadikey_on_boarding_viewpager = (ViewPager) findViewById(R.id.gaadikey_on_boarding_viewpager);
+        on_boarding_indicator_image1 = (ImageView) findViewById(R.id.on_boarding_indicator_image1);
+        on_boarding_indicator_image2 = (ImageView) findViewById(R.id.on_boarding_indicator_image2);
+        on_boarding_indicator_image3 = (ImageView) findViewById(R.id.on_boarding_indicator_image3);
         gaadikey_pager_adapter = new GaadiKeyPagerAdapter(getSupportFragmentManager());
         gaadikey_on_boarding_viewpager.setAdapter(gaadikey_pager_adapter);
+        gaadikey_on_boarding_viewpager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                adjustIndicator(position);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
     }
+
+    public void adjustIndicator(int currentScreenPosition) {
+        switch (currentScreenPosition) {
+            case 0:
+                on_boarding_indicator_image1.setImageResource(R.drawable.on_boarding_indicator2);
+                on_boarding_indicator_image2.setImageResource(R.drawable.on_boarding_indicator1);
+                on_boarding_indicator_image3.setImageResource(R.drawable.on_boarding_indicator1);
+                break;
+            case 1:
+                on_boarding_indicator_image1.setImageResource(R.drawable.on_boarding_indicator1);
+                on_boarding_indicator_image2.setImageResource(R.drawable.on_boarding_indicator2);
+                on_boarding_indicator_image3.setImageResource(R.drawable.on_boarding_indicator1);
+                break;
+            case 2:
+                on_boarding_indicator_image1.setImageResource(R.drawable.on_boarding_indicator1);
+                on_boarding_indicator_image2.setImageResource(R.drawable.on_boarding_indicator1);
+                on_boarding_indicator_image3.setImageResource(R.drawable.on_boarding_indicator2);
+                break;
+        }
+    }
+
+
 }
